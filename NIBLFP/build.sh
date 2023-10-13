33a40631db2af57cf92649f054c969c85900b41f
Erich Küster <erich.kuester@arcor.de>
2023-10-13 10:09:13 +0200

    NIBLFP revert to v1.1.10

--- NIBLFP/build.sh
+++ NIBLFP/build.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 # revised listing
 # asl should reside in parent directory, otherwise adapt commands
-../asl -cpu sc/mp -L NIBLFP.asm &&
-../p2bin NIBLFP -r '53248-$' &&
-../p2hex NIBLFP -r '53248-$' -F Intel -l 32 &&
-rm NIBLFP.p
+fn=NIBLFP
+defs="-D BAUD=1200"
+#defs="-D EMULA"
+../asl -cpu sc/mp ${defs} -L ${fn}.asm &&
+../p2bin ${fn} -r '53248-$' &&
+../p2hex ${fn} -r '53248-$' -F Intel -l 32 &&
+rm ${fn}.p
 
