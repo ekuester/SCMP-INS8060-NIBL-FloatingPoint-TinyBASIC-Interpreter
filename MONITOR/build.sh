7ccf0cce384ede15764609321219b7ca8a582440
Erich Küster <erich.kuester@arcor.de>
2023-11-01 17:14:55 +0100

    rewritten v1.2.2

--- MONITOR/build.sh
+++ MONITOR/build.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 # revised listing
 # asl should reside in parent directory, otherwise adapt commands
-../asl -cpu sc/mp -L MONITOR.asm &&
-../p2bin MONITOR -r '49152-$' &&
-../p2hex MONITOR -r '49152-$'  -F Intel -l 32 &&
-rm MONITOR.p
+# use './build.sh NIBLFP' to build the binary, for emulation uncomment line 6
+fn=$1
+#defs="-D EMULA"
+../asl -cpu sc/mp ${defs} -L ${fn}.asm &&
+../p2bin ${fn} -r '49152-$' &&
+../p2hex ${fn} -r '49152-$'  -F Intel -l 32 &&
+rm ${fn}.p
